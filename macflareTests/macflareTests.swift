8f2efc18139953500afe23394b829c90d0e2b86e
Peter C. Allport <6022036+peteallport@users.noreply.github.com>
2025-06-24 17:06:23 -0700

    Initial commit: Add macflare Xcode project (#1)
    Add initial macflare Xcode project with SwiftUI app, model, assets, configuration templates, and test targets. Includes .gitignore, license, and documentation for setup and contribution.

---
+++ macflareTests/macflareTests.swift
@@ -0,0 +1,16 @@
+//
+//  macflareTests.swift
+//  macflareTests
+//
+//  Created by Peter C. Allport on 6/24/25.
+//
+
+import Testing
+
+struct macflareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
